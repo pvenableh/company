5ed94d2e640ae64d38899ffa7c6c6811495b047a
Peter Hoffman <peter@huestudios.com>
2024-03-22 19:43:41 -0400

    Adjusted packages.

--- components/Account/Logout.vue
+++ components/Account/Logout.vue
@@ -1,30 +1,13 @@
 <template>
-	<a class="cursor-pointer" @click.prevent="submit">Logouts</a>
+	<a class="cursor-pointer" @click.prevent="handleLogout">Logout</a>
 </template>
 <script setup>
-import { createDirectus, rest, logout } from '@directus/sdk';
+const { logout } = useDirectusAuth();
 const cookie = useCookie('directus_refresh_token');
 
-const password = ref();
-const client = createDirectus('https://admin.huestudios.company').with(rest());
-
-async function submit() {
-	// const result = await client.request(logout());
-	const result = await client.request(logout({ refresh_token: cookie.value }));
-	console.log(result);
-}
-// const { logout } = useDirectusAuth();
-
-// const onSubmit = async () => {
-// 	await logout();
-// 	return await navigateTo('/', { replace: true });
-// };
-// const cookie = useCookie('directus_refresh_token');
-// const { logout } = useDirectusAuth();
-
-// const handleLogout = async () => {
-// 	console.log(cookie.value);
-// 	await logout(cookie.value);
-// };
+const handleLogout = async () => {
+	await logout(cookie.value);
+	return await navigateTo('/', { replace: true });
+};
 </script>
 <style></style>
