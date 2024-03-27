e0054f4a9ee0728e62b6b365aebe1b1df065a658
Peter Hoffman <peter@huestudios.com>
2024-03-27 11:12:23 -0400

    Updated packages.

--- components/Account/Logout.vue
+++ components/Account/Logout.vue
@@ -2,12 +2,24 @@
 	<a class="cursor-pointer" @click.prevent="handleLogout">Logout</a>
 </template>
 <script setup>
-const { logout } = useDirectusAuth();
+// const { logout } = useDirectusAuth();
 const cookie = useCookie('directus_refresh_token');
 
+// const handleLogout = async () => {
+// 	await logout(cookie.value);
+// 	return await navigateTo('/', { replace: true });
+// };
+
+import { createDirectus, authentication, rest, logout } from '@directus/sdk';
+
+const client = createDirectus('https://admin.huestudios.company').with(authentication()).with(rest());
 const handleLogout = async () => {
-	await logout(cookie.value);
-	return await navigateTo('/', { replace: true });
+	// logout using the authentication composable
+	// const result = await client.logout();
+
+	// // logout http request
+	console.log(cookie.value);
+	const result = await client.request(logout(cookie.value));
 };
 </script>
 <style></style>
