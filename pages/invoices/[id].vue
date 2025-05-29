74f280d13ac9ddd5363bde416ffcbc536148b6ff
Peter Hoffman <peter@huestudios.com>
2025-05-29 18:01:30 -0400

    Added melio option.

--- pages/invoices/[id].vue
+++ pages/invoices/[id].vue
@@ -19,7 +19,7 @@ definePageMeta({
 
 const invoice = await readItem('invoices', params.id, {
 	fields: [
-		'id,status,due_date,invoice_date,invoice_code,note,memo,total_amount,bill_to.id,bill_to.name,bill_to.email,bill_to.emails,bill_to.stripe_customer_id,bill_to.address,line_items.id,line_items.description,line_items.quantity,line_items.rate,line_items.amount,line_items.product.name,payments.*',
+		'id,status,due_date,invoice_date,invoice_code,note,memo,total_amount,bill_to.id,bill_to.name,bill_to.email,bill_to.emails,bill_to.stripe_customer_id,bill_to.address,line_items.id,line_items.description,line_items.quantity,line_items.rate,line_items.amount,line_items.product.name,payments.*,melio',
 	],
 });
 
@@ -73,9 +73,32 @@ const handleAnonymousSubmit = async (formData) => {
 		<div class="w-full flex flex-col lg:flex-row items-center lg:items-start justify-center z-10 page__inner">
 			<InvoicesInvoice :invoice="invoice" class="lg:sticky lg:top-12" />
 			<div v-if="showAnonymousForm && invoice.status === 'pending'" class="w-full px-6 pt-0 pb-16 lg:w-1/2 max-w-xl">
+				<UButton
+					v-if="invoice.melio"
+					:to="invoice.melio"
+					target="_blank"
+					class="w-full mb-6"
+					color="gray"
+					variant="ghost"
+					size="sm"
+				>
+					Pay with Melio
+				</UButton>
 				<PaymentAnonymous :default-email="defaultEmail" @submit="handleAnonymousSubmit" />
 			</div>
+
 			<div v-else-if="invoice.status === 'pending'" class="w-full px-6 pt-0 pb-16 lg:w-1/2 max-w-xl">
+				<UButton
+					v-if="invoice.melio"
+					:to="invoice.melio"
+					target="_blank"
+					class="w-full mb-6"
+					color="gray"
+					variant="solid"
+					size="sm"
+				>
+					Pay with Melio
+				</UButton>
 				<PaymentMethods
 					:amount="invoice.total_amount"
 					:email="user ? user.email : anonymousUser.email"
